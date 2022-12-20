dde2b9c89bfb44c0d32a19e3b178cf71d4113ed5
Homero Ochoa <homeroochoa47@gmail.com>
2022-12-19 17:42:50 -0800

    formatting song card

--- frontend/src/components/cards/songCard.js
+++ frontend/src/components/cards/songCard.js
@@ -12,54 +12,27 @@ export function SongCard(props) {
             <Image
                 objectFit='cover'
                 maxW={{ base: '100%', sm: '200px' }}
-                src='https://images.unsplash.com/photo-1667489022797-ab608913feeb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw5fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=800&q=60'
-                alt='Caffe Latte'/>
+                src='' //album cover link here
+                alt=''/>
             <Stack>
                 <CardBody>
-                    <Heading size='md'>The perfect latte</Heading>
+                    <Heading size='md'>Song Name</Heading>
                     <Text py='2'>
-                        Caffè latte is a coffee beverage of Italian origin made with espresso
-                        and steamed milk.
+                        Artist
+                    </Text>
+                    <Text py='2'>
+                        Album Name
+                    </Text>
+                    <Text py='2'>
+                        Release Date
                     </Text>
                 </CardBody>
                 <CardFooter>
                     <Button variant='solid' colorScheme='blue'>
-                        Buy Latte
+                        More about this artist
                     </Button>
                 </CardFooter>
             </Stack>
         </Card>
     )
 };
-
-
-
-<Card
-  direction={{ base: 'column', sm: 'row' }}
-  overflow='hidden'
-  variant='outline'
->
-  <Image
-    objectFit='cover'
-    maxW={{ base: '100%', sm: '200px' }}
-    src='https://images.unsplash.com/photo-1667489022797-ab608913feeb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw5fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=800&q=60'
-    alt='Caffe Latte'
-  />
-
-  <Stack>
-    <CardBody>
-      <Heading size='md'>The perfect latte</Heading>
-
-      <Text py='2'>
-        Caffè latte is a coffee beverage of Italian origin made with espresso
-        and steamed milk.
-      </Text>
-    </CardBody>
-
-    <CardFooter>
-      <Button variant='solid' colorScheme='blue'>
-        Buy Latte
-      </Button>
-    </CardFooter>
-  </Stack>
-</Card>
